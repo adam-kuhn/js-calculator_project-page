ad7901563a47f00b6c18b88aac10d6198c081549
adam-kuhn <adam.kuhn20@gmail.com>
2017-12-31 13:36:34 +1300

    tidying JS, removed redundant number functions

--- calc-js.js
+++ calc-js.js
@@ -1,16 +1,12 @@
+var nbrButtons = document.getElementsByClassName("nbr-btn") //access number buttons on calculator
 
 window.onload = function (){
 
-document.getElementById("0").addEventListener("click", display0);
-document.getElementById("1").addEventListener("click", display1);
-document.getElementById("2").addEventListener("click", display2);
-document.getElementById("3").addEventListener("click", display3);
-document.getElementById("4").addEventListener("click", display4);
-document.getElementById("5").addEventListener("click", display5);
-document.getElementById("6").addEventListener("click", display6);
-document.getElementById("7").addEventListener("click", display7);
-document.getElementById("8").addEventListener("click", display8);
-document.getElementById("9").addEventListener("click", display9);
+for (n=0; n<nbrButtons.length; n++){
+  nbrButtons[n].addEventListener('click', displayNum)
+}
+
+
 
 document.getElementById("plus").addEventListener("click", displayPlus);
 document.getElementById("minus").addEventListener("click", displayMinus);
@@ -19,8 +15,7 @@ document.getElementById("divide").addEventListener("click", displayDivide);
 document.getElementById("equals").addEventListener("click", displayAnswer);
 document.getElementById("clear").addEventListener("click", clear);
 
-let digits = document.getElementsByClassName("nbr-btn")
-let operator = document.getElementsByClassName("opr-btn")
+
 }
 let equation = []
 let operator = []
@@ -38,65 +33,14 @@ function displayEquation(){
     return document.getElementById("calc-equation").innerHTML = "screen too small!"
   }
 }
-function display0(){
-  document.getElementById("calc-screen").innerHTML = 0
-  equation.push(0)
-  displayEquation()
 
-}
-function display1(){
-  document.getElementById("calc-screen").innerHTML = 1
-  equation.push(1)
+function displayNum(e){
+  var id = e.target.id;
+  document.getElementById("calc-screen").innerHTML = id
+  equation.push(id)
   displayEquation()
 }
 
-function display2(){
-  document.getElementById("calc-screen").innerHTML = 2
-  equation.push(2)
-  displayEquation()
-}
-
-function display3(){
-  document.getElementById("calc-screen").innerHTML = 3
-  equation.push(3)
-  displayEquation()
-}
-
-function display4(){
-  document.getElementById("calc-screen").innerHTML = 4
-  equation.push(4)
-  displayEquation()
-}
-
-function display5(){
-  document.getElementById("calc-screen").innerHTML = 5
-  equation.push(5)
-  displayEquation()
-}
-
-function display6(){
-  document.getElementById("calc-screen").innerHTML = 6
-  equation.push(6)
-  displayEquation()
-}
-
-function display7(){
-  document.getElementById("calc-screen").innerHTML = 7
-  equation.push(7)
-  displayEquation()
-}
-
-function display8(){
-  document.getElementById("calc-screen").innerHTML = 8
-  equation.push(8)
-  displayEquation()
-}
-
-function display9(){
-  document.getElementById("calc-screen").innerHTML = 9
-  equation.push(9)
-  displayEquation()
-}
 
 //operator functions
 function displayPlus(){
